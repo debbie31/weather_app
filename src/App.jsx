89e4664e648c2d3ebd54709db304ebad9e2b181b
debbie31 <sanchezdebzy@gmail.com>
2022-12-18 16:52:40 +0800

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Wrap from './components/Wrap';
+import Weather from './components/Weather/Index';
+
+const App = () => {
+  return (
+    <Wrap>
+      <Weather />
+    </Wrap>
+  );
+}
+
+export default App;
